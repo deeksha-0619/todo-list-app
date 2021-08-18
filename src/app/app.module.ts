6ab623b5365cfca53031af6990e866d98d8b4a7d
Deeksha Rani <Deeksharani0619@gmail.com>
2021-08-18 23:52:09 +0530

    Final Updates to todo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 
@@ -13,7 +12,6 @@ import { TodoComponent } from './components/todo/todo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule
   ],
